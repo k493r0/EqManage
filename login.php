e043d696ac45980aa0a517251dbda8d38de0f794
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2019-12-16 23:21:09 +0800

    added basic login field

--- login.php
+++ login.php
@@ -14,7 +14,53 @@
 </head>
 
 
+<body>
 
+<div class="page-content">
+    <div>
+
+        <div>
+            <label class="form-row-inner">
+                <span class="label">Full Name</span>
+                <input type="text" name="fullname" id="full_name" class="input-text" required>
+
+            </label>
+        </div>
+
+        <div>
+            <label class="form-row-inner">
+                <span class="label">Username</span>
+                <input type="text" name="fullname" id="full_name" class="input-text" required>
+
+            </label>
+        </div>
+
+        <div>
+            <label class="form-row-inner">
+                <span class="label">Email</span>
+                <input type="text" name="fullname" id="full_name" class="input-text" required>
+
+            </label>
+        </div>
+
+
+        <div>
+            <label class="form-row-inner">
+                <span class="label">Password</span>
+                <input type="text" name="fullname" id="full_name" class="input-text" required>
+
+            </label>
+        </div>
+
+        <div>
+            <input type="submit" name="reg-user" class="register" value="Register">
+        </div>
+
+    </div>
+
+</div>
+
+</body>
 
 
 </html>
