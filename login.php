bbd29cf6865c7316bf39df1f4f8972975e8ef902
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2019-12-16 23:52:07 +0800

    added basic login form

--- login.php
+++ login.php
@@ -13,6 +13,41 @@
 
 <body>
 
+<div>
+    <div>
+        <label>
+            <span class="label">Full Name</span>
+            <input type="text" name="fullname" id="full_name" class="input-text" required>
+        </label>
+    </div>
+    <div>
+        <label>
+            <span class="label">Username</span>
+            <input type="text" name="username" id="full_name" class="input-text" required>
+        </label>
+    </div>
+    <div>
+        <label>
+            <span class="label">Email</span>
+            <input type="text" name="email" id="full_name" class="input-text" required>
+        </label>
+    </div>
+    <div>
+        <label>
+            <span class="label">Password</span>
+            <input type="password" name="password_1" id="full_name" class="input-text" required>
+        </label>
+    </div>
+    <div>
+        <label>
+            <span class="label">Confirm Password</span>
+            <input type="password" name="password" id="full_name" class="input-text" required>
+        </label>
+    </div>
+    <div>
+        <input type="submit" name="reg_user" class="register" value="Register">
+    </div>
+</div>
 
 
 </body>
