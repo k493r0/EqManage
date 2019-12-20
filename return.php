7204b8498955cfdf0157ef37475c5807fa1a663e
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2019-12-20 17:34:19 +0800

    added return system, optimised server access + fixed barcode assets

--- return.php
+++ return.php
@@ -1,1 +1,76 @@
-<?php ?>
+<?php
+session_start();
+if(!isset($_SESSION['loggedin'])){
+header('Location: index.php');
+exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Media Team Equipment Management System</title>
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Font-->
+    <link rel="stylesheet" type="text/css" href="assets/css/sourcesanspro-font.css">
+    <!-- Main Style Css -->
+    <link rel="stylesheet" href="assets/css/loaderstyle.css"/>
+    <link rel="stylesheet" href="assets/css/corestyle.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <script src="assets/js/scripts.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script>
+        $(function () {
+            setTimeout(function () {
+                $("#loader").fadeOut(500);
+            }, 1500)
+        })
+
+    </script>
+</head>
+
+
+<body class="form-v8 loggedin" id="fade">
+
+<div id="loader">
+    <div class="loader"><div></div><div></div><div></div><div></div></div>
+</div>
+
+<?php include('navbar.php'); ?>
+
+<div class="content">
+    <div>
+        <h2>Return</h2>
+
+        <p>
+            <?php
+            include('serverconnect.php');
+            $name = $_SESSION['name'];
+            $resultset = mysqli_query($db, "select Equipment from equipments where Availability = 0 and Name='$name'");
+            ?>
+        <div class="select-style" style="width:500px; margin: auto">
+            <form action="confirmation.php" style="width: 100%;" method="POST">
+                <select name="Equipments" class="select-picker" >
+                    <option value="" disabled selected>Select the Equipment</option>
+                    <?php
+                    while ($row = mysqli_fetch_array($resultset)){
+                        $Equipment = $row['Equipment'];
+                        echo "<option value='$Equipment' >$Equipment</option>";
+                    }
+                    ?>
+                </select>
+                <input name="request" type="submit" value="Return" style="width: 100%; margin-top: 20px">
+            </form>
+        </div>
+        </p>
+
+    </div>
+
+</div>
+
