6b4c0e33a7e50481b1f33de295751ecd63dea814
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-01-22 21:13:44 +0800

    updated server

--- serverconnect.php
+++ serverconnect.php
@@ -8,4 +8,11 @@ $email    = "";
 $errors = array();
 
 // connect to the database
-$db = mysqli_connect('remotemysql.com', 'tgsK9nTZNV', 'UFJLMZcF2L', 'tgsK9nTZNV');
+//$db = mysqli_connect('remotemysql.com', 'tgsK9nTZNV', 'UFJLMZcF2L', 'tgsK9nTZNV');
+$db = mysqli_connect('remotemysql.com', 'aJNhE8Tihv', '9gY0DX3OdL', 'aJNhE8Tihv');
+
+if ( mysqli_connect_errno() ) {
+    // If there is an error with the connection, stop the script and display the error.
+    die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
--- signup.php
+++ signup.php
@@ -8,7 +8,7 @@ $email    = "";
 $errors = array();
 
 // connect to the database
-$db = mysqli_connect('remotemysql.com', 'tgsK9nTZNV', 'UFJLMZcF2L', 'tgsK9nTZNV');
+$db = mysqli_connect('bowij1jp1qu5kwy79igt-mysql.services.clever-cloud.com', 'u9tgb8b4hu8gxlsy', 'tViTNqqvk2MwXcFx2Gj5', 'bowij1jp1qu5kwy79igt');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -31,7 +31,7 @@ if (isset($_POST['reg_user'])) {
 
     // first check the database to make sure
     // a user does not already exist with the same username and/or email
-    $user_check_query = "SELECT * FROM accounts WHERE username='$username' OR email='$email' LIMIT 1";
+    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
 
@@ -49,7 +49,7 @@ if (isset($_POST['reg_user'])) {
     if (count($errors) == 0) {
         $password = password_hash($password_1, PASSWORD_DEFAULT);//encrypt the password before saving in the database
 
-        $query = "INSERT INTO accounts (fullname,username, email, password) 
+        $query = "INSERT INTO users (fullname,username, email, password) 
   			  VALUES('$fullname','$username', '$email', '$password')";
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
