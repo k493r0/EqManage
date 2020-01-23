16af2692d7e5e15077f0d4d4a4247f268847e156
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-01-23 17:49:48 +0800

    complted fetchCategory.php

--- fetchCategory.php
+++ fetchCategory.php
@@ -1,1 +1,20 @@
 <?php
+
+include ("serverconnect.php");
+
+$categoryid = $_POST['category_id'];
+
+$query = "SELECT * FROM EqManage.categories WHERE id=".$categoryid;
+
+$result = mysqli_query($db,$query);
+
+$html = '<div>';
+while($row = mysqli_fetch_array($result)){
+    $name = $row['categoryName'];
+
+    $html .= "<span class='head'>Category Name : </span><span>".$name."</span><br/>";
+
+}
+$html .= '</div>';
+
+echo $html;
