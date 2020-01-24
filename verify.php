dc18d564dde4f725bec81a9579a608367beb683e
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-01-24 21:28:17 +0800

    fixed query statements + added qty subtraction query

--- verify.php
+++ verify.php
@@ -5,15 +5,17 @@ if(!isset($_SESSION['loggedin'])){
     header('Location: login.php');
     exit();
 }
+
+
         $string1 = $_SERVER['QUERY_STRING'];
         $string2 = str_replace("hash=","",$string1);
         echo $string2;
 
-        $query = "UPDATE EqManage.requests SET Active='1' WHERE Hash='$string2'";
+        $query = "UPDATE EqManage.requests SET active='1',state='approved' WHERE hash='$string2'";
         mysqli_query($db,$query);
 
 
-        $request_query = "Select * from EqManage.requests where Hash='$string2'";
+        $request_query = "Select * from EqManage.requests where hash='$string2'";
         $result = mysqli_query($db,$request_query);
         $request = mysqli_fetch_assoc($result);
 //        echo $request['equipment_id'];
@@ -29,8 +31,7 @@ if(!isset($_SESSION['loggedin'])){
         $expectedReturnDate = $request['expectedReturnDate'];
         $date = date('Y-m-d H:i:s');
 
-        $minusQty = 1;
-
+        $checkoutQty = $request['checkoutQty'];
 
 
         $log_query = "INSERT into EqManage.log (checkoutRequests_id, equipment_id,users_id,notes,checkoutDate,expectedReturnDate) values ('$checkoutRequest_id','$equipment_id','$users_id','$note','$date', '$expectedReturnDate')";
@@ -52,11 +53,10 @@ $checkQtyArray = mysqli_fetch_assoc($result2);
 $leftQty = $checkQtyArray['leftQuantity'];
 echo "left qty: ", $leftQty, "------";
 echo $minusQty;
-$newleftQty = $leftQty - $minusQty;
+$newleftQty = $leftQty - $checkoutQty;
 echo $newleftQty;
 if ($newleftQty<=0){
     $updateEq_query = "UPDATE EqManage.equipment SET availability=0,users_id='$users_id',lastLog_id='$last_id',leftQuantity='$newleftQty' WHERE id='$equipment_id'";
-
 } else $updateEq_query = "UPDATE EqManage.equipment SET users_id='$users_id',lastLog_id='$last_id',leftQuantity='$newleftQty' WHERE id='$equipment_id'";
 
 
