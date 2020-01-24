b5a485407d28673a68b9c169da37c771af91462c
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-01-24 23:54:33 +0800

    working confirmation screen with id querying + fixed option selection on return.php

--- return.php
+++ return.php
@@ -29,16 +29,45 @@ include('header.php')
             <?php
             include('serverconnect.php');
             $name = $_SESSION['name'];
-            $resultset = mysqli_query($db, "select Equipment from equipments where Availability = 0 and Name='$name'");
+            $user_id = $_SESSION['id'];
+            $resultset = mysqli_query($db, "
+select l.equipment_id, l.returnDate, E.equipment, e.leftQuantity,l.users_id
+from EqManage.log L 
+left JOIN EqManage.equipment E on L.equipment_id = E.id 
+where l.returnDate is null
+");
+            echo $user_id;
+//            while ($row = mysqli_fetch_array($resultset)){
+//                if($row['users_id'] = $user_id){
+//                    echo $row['equipment_id'];
+//                    $equipmentName = $row['equipment'];
+//                    $equipmentID = $row['equipment_id'];
+//                    echo "<option value='$equipmentID' >$equipment</option>";
+//                }
+//
+//
+//
+//            }
+
+
+
+
             ?>
         <div class="select-style" style="width:500px; margin: auto">
             <form action="confirmation.php" style="width: 100%;" method="POST">
-                <select name="Equipments" class="select-picker" >
+                <select name="equipment" class="select-picker" >
                     <option value="" disabled selected>Select the Equipment</option>
                     <?php
                     while ($row = mysqli_fetch_array($resultset)){
-                        $Equipment = $row['Equipment'];
-                        echo "<option value='$Equipment' >$Equipment</option>";
+                        if($row['users_id'] = $user_id){
+                            echo $row['equipment_id'];
+                            $equipmentName = $row['equipment'];
+                            $equipmentID = $row['equipment_id'];
+                            echo "<option value='$equipmentID' >$equipmentName</option>";
+                        }
+
+
+
                     }
                     ?>
                 </select>
