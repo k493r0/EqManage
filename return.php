7300ba53ff9a07a533c30d1c2fe9b924aaa1a574
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-01-25 00:54:02 +0800

    working flow from adding equipment -> single check out -> verification via postverify.php -> successful data entry in checkout -> return (single) -> successful data entry in return *without redirection

--- return.php
+++ return.php
@@ -31,7 +31,7 @@ include('header.php')
             $name = $_SESSION['name'];
             $user_id = $_SESSION['id'];
             $resultset = mysqli_query($db, "
-select l.equipment_id, l.returnDate, E.equipment, e.leftQuantity,l.users_id
+select l.equipment_id, l.returnDate, E.equipment, e.leftQuantity,l.users_id,l.checkoutRequests_id
 from EqManage.log L 
 left JOIN EqManage.equipment E on L.equipment_id = E.id 
 where l.returnDate is null
@@ -55,15 +55,17 @@ where l.returnDate is null
             ?>
         <div class="select-style" style="width:500px; margin: auto">
             <form action="confirmation.php" style="width: 100%;" method="POST">
-                <select name="equipment" class="select-picker" >
-                    <option value="" disabled selected>Select the Equipment</option>
+                <select name="equipment" class="select-picker" onchange="getID(this)">
+                    <option value=""  disabled selected>Select the Equipment</option>
                     <?php
                     while ($row = mysqli_fetch_array($resultset)){
                         if($row['users_id'] = $user_id){
                             echo $row['equipment_id'];
                             $equipmentName = $row['equipment'];
                             $equipmentID = $row['equipment_id'];
-                            echo "<option value='$equipmentID' >$equipmentName</option>";
+                            $checkoutRequestsID = $row['checkoutRequests_id'];
+                            echo $checkoutRequestsID;
+                            echo "<option value='$equipmentID' data-checkoutRequestsID='$checkoutRequestsID'>$equipmentName</option>";
                         }
 
 
@@ -71,6 +73,7 @@ where l.returnDate is null
                     }
                     ?>
                 </select>
+                <input type="hidden" id="checkoutRequestsID" name="checkoutRequestsID" value="">
                 <input name="request" type="submit" value="Return" style="width: 100%; margin-top: 20px">
             </form>
         </div>
@@ -79,4 +82,11 @@ where l.returnDate is null
     </div>
 
 </div>
+<script>
+    function getID(id) {
+        id = id.options[id.selectedIndex].getAttribute('data-checkoutRequestsID');
+        console.log(id);
+        document.getElementById("checkoutRequestsID").setAttribute("value",id);
 
+    }
+</script>
