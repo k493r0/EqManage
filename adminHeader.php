a55d9178417a5ec140d612a02f9df1bbaf774ea3
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-03-20 16:13:36 +0800

    uodated dashboardstyle.css + added adminHeader.php + dashboard.php placeholder added

---
+++ adminHeader.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="utf-8">
+    <title>Media Team Equipment Management System</title>
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Font-->
+    <link rel="stylesheet" type="text/css" href="assets/css/sourcesanspro-font.css">
+    <!-- Main Style Css -->
+    <link rel="stylesheet" href="assets/css/loaderstyle.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/tablestyle.css">
+    <link rel="stylesheet" href="assets/css/modalstyle.css">
+    <script src="assets/js/scripts.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+    <script>
+        $(function () {
+            setTimeout(function () {
+                $("#loader").fadeOut(500);
+            }, 1500)
+        })
+
+    </script>
+
+
+
+</head>
