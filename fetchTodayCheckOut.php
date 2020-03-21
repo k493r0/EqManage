6f25e004c3a35b35c830d51fad5df57f63c57ff3
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-03-21 17:10:07 +0800

    added fetchTodayCheckOut.php

---
+++ fetchTodayCheckOut.php
@@ -0,0 +1,49 @@
+<?php
+include('serverconnect.php');
+$query = mysqli_query($db, "Select * from EqManage.log");
+$query2 = mysqli_query($db, "Select * from EqManage.requests");
+$today = date("Y-m-d H:i:s");
+$todayExplode = explode(" ", $today);
+//echo $todayExplode[0];
+
+$monthago = date('Y-m-d', strtotime('now - 1 month'));
+//echo $monthago;
+
+$overdue = 0;
+$checkoutToday = 0;
+$pendingRQ = 0;
+$checkoutMonth = 0;
+
+//                echo $today;
+while ($row = mysqli_fetch_array($query)) {
+    $returnDate = $row['expectedReturnDate'];
+    $checkoutDate = $row['checkoutDate'];
+    $checkoutDateExplode = explode(" ", $checkoutDate);
+
+//                    echo "<p>'$returnDate'</p>";
+    if (strtotime($returnDate) < strtotime($today)) {
+        $overdue++;
+    }
+    if (strtotime($checkoutDateExplode[0]) == strtotime($todayExplode[0])) {
+        $checkoutToday++;
+    }
+    if (strtotime($checkoutDateExplode[0]) >= strtotime($monthago)) {
+        $checkoutMonth++;
+    }
+};
+
+
+while ($row = mysqli_fetch_array($query2)) {
+    $state = $row['state'];
+    if ($state == null) {
+        $pendingRQ++;
+    }
+}
+
+//echo "Overdue: ", $overdue;
+//echo " Checked out today: ", $checkoutToday;
+//echo " Pending requests: ", $pendingRQ;
+//echo " Checked out this month", $checkoutMonth;
+echo "<h3 class=\"card-title\">",$checkoutToday,"</h3>";
+
+
