b9f898928174b345e76c6ed5fc9696c37ea1f5b4
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-03-21 15:05:06 +0800

    admin checkout system working

--- adminCheckout.php
+++ adminCheckout.php
@@ -1,15 +1,25 @@
 <?php
-include('serverconnect.php');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userID = $_POST['studentselect'];
-    $eqID = $_POST['eqselect'];
-    $requestOption = $_POST['checkOutSelect'];
+    include('serverconnect.php');
+    $today = date('Y-m-d H:i:s');
+    $userID = $_POST['userID'];
+    $eqID = $_POST['eqID'];
+    $requestOption = $_POST['checkoutID'];
 
     echo $userID;
     echo $eqID;
     echo $requestOption;
 
+    if ($requestOption == 0) {
+        $query = "UPDATE EqManage.log set checkoutDate = '$today' where users_id = '$userID' AND checkoutDate IS NULL AND returnDate IS NULL";
+    } else $query = "UPDATE EqManage.log set checkoutDate = '$today' where checkoutRequests_id = '$requestOption'";
 
 
+    if (mysqli_query($db, $query)) {
+        echo "Successfully updated table";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($db);
+    }
 
 }
