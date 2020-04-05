27f0844e42b4c2251eb21b7e5ce01fab72928663
Yuki Kume <yuki.kume@dulwich-suzhou.cn>
2020-04-05 22:00:44 +0800

    working tempSearch with User search and Equipment Search

--- search.php
+++ search.php
@@ -22,7 +22,8 @@ include('serverconnect.php');
     <link rel="stylesheet" href="assets/css/select2.min.css"/>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="bootstrap-iso.css">
     <script src="assets/js/select2.min.js"></script>
 
@@ -38,7 +39,7 @@ include('serverconnect.php');
         <?php include('adminNavbar.php')?>
         <!-- End Navbar -->
 
-        <div class="content ">
+        <div class="content">
             <h2 class="text-center">Search</h2>
 
 
@@ -47,19 +48,61 @@ include('serverconnect.php');
                 <!-- your content here -->
 
                 <ul class="nav nav-tabs">
-                    <li class="active"><a data-toggle="tab" href="#user">User</a></li>
-                    <li><a data-toggle="tab" href="#eq">Equipment</a></li>
+                    <li class="active"><a data-toggle="tab" data-target="#user, #user_button" href=".user">User</a></li>
+                    <li><a data-toggle="tab" data-target="#eq, #eq_button" href=".eq">Equipment</a></li>
                     <li><a data-toggle="tab" href="#cat">Category</a></li>
                     <li><a data-toggle="tab" href="#log">Log</a></li>
                     <li><a data-toggle="tab" href="#rq">Requests</a></li>
                 </ul>
 
-            <div class="tab-content">
-                <?php include('searchUser.php'); ?>
-<!--                --><?php //include('searchEq.php'); ?>
+                <div class="tab-content">
+
+
+                    <?php include('searchUser.php'); ?>
+
+
+                    <?php include('searchEq.php'); ?>
+
+
+
+                </div>
+
+                <ul id="myTab" class="nav nav-tabs">
+                    <li class="active"><a href="#home" data-target="#home, #home_else" data-toggle="tab">C1</a></li>
+                    <li><a href="#profile" data-target="#profile, #profile_else" data-toggle="tab">C2</a></li>
+                </ul>
+
+                <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane fade in active bootstrap-iso" id="home" role="tabpanel">
+                        <p>Content 1.</p>
+                    </div>
+                    <div class="tab-pane fade bootstrap-iso" id="profile" role="tabpanel">
+                        <p>Content 2.</p>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane fade in active bootstrap-iso" id="home_else" role="tabpanel">
+                        <p>Content 111.</p>
+                    </div>
+                    <div class="tab-pane fade bootstrap-iso" id="profile_else" role="tabpanel">
+                        <p>Content 2222.</p>
+                    </div>
+                </div>
+
+
             </div>
+
+
+
+
+
+
         </div>
     </div>
+
 </div>
 
 </body>
@@ -77,4 +120,25 @@ include('serverconnect.php');
         allowClear: true,
 
     } );
+    $(document).ready(function() {
+
+        $("#userSelect").change(function () {
+            var id = $(this).val();
+            console.log("Working");
+
+            var url = 'fetchSearchUser.php?' + 'id=' + id;
+            console.log(url);
+
+            $("#user").load(url);
+            console.log("Done");
+
+        })});
+
+    $("#userSelect").select2( {
+        placeholder: "Enter user ID",
+        allowClear: true,
+
+    } );
+
+
 </script>
--- searchUser.php
+++ searchUser.php
@@ -1,28 +1,28 @@
-<div style="position: center" align="center" id="searchUser">
-    <label for="userSelect" style="margin-top: 20px">Search:</label>
-    <select id="userSelect" style="width: 20%; text-align: left;margin-bottom: 10px" >
-        <div id="userSelectDiv">
+<div id="user_button" class="tab-pane fade in active bootstrap-iso user" style="margin-top: 10px">
+    <div style="position: center" align="center" id="searchUser">
+        <label for="userSelect" style="margin-top: 20px">Search:</label>
+        <select id="userSelect" style="width: 20%; text-align: left;margin-bottom: 10px" onchange="change()">
 
-            <?php include('fetchCheckoutEq.php');
+                <?php
+                $returnResult = mysqli_query($db,"select * from EqManage.users");
+                while ($row = mysqli_fetch_array($returnResult)){
 
-            $returnResult = mysqli_query($db,"select * from EqManage.users");
-            while ($row = mysqli_fetch_array($returnResult)){
+                    echo "<option value=\"\">Select User</option>";
+                    $userID = $row['id'];
+                    $fullname = $row['fullname'];
+                    echo "<option value='$userID'>ID: $userID | Name: $fullname</option>";
+                };
+                ?>
 
-            echo "<option value=\"\">Select User</option>";
-            $userID = $row['id'];
-            $fullname = $row['fullname'];
-            echo "<option value='$userID'>ID: $userID | Name: $fullname</option>";
-};
-            ?>
-        </div>
-
-    </select>
+        </select>
+    </div>
 </div>
 
-
-<div id="user" class="tab-pane fade in active bootstrap-iso" style="margin-top: 10px">
-<?php  include('fetchSearchUser.php') ?>
-
+<div id="user" class="tab-pane fade in active bootstrap-iso user" style="margin-top: 10px">
+        <?php
+        include ('fetchSearchUser.php');
+        ?>
+    </div>
 </div>
 
 
@@ -47,4 +47,16 @@
         allowClear: true,
 
     } );
+
+    function change() {
+        var e = document.getElementById("userSelect");
+        var id = e.options[e.selectedIndex].value;
+        console.log(id);
+
+        var url = 'fetchSearchUser.php?' + 'id=' + id;
+        console.log(url);
+
+        $("#user").load(url);
+        console.log("Done");
+    }
 </script>
