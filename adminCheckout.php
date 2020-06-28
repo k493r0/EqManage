08bd0432ebe0d76712704aa5adc86e8459cf0076
Wren Hainsworth <hainsworthwren@gmail.com>
2020-06-28 18:06:17 +0800

    working checkout-return loop

--- adminCheckout.php
+++ adminCheckout.php
@@ -12,11 +12,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo $requestOption;
 
     if ($requestOption == 0) {
-        $query = "UPDATE EqManage.log set checkoutDate = '$today' where users_id = '$userID' AND checkoutDate IS NULL AND returnDate IS NULL";
-    } else $query = "UPDATE EqManage.log set checkoutDate = '$today' where checkoutRequests_id = '$requestOption'";
+        $query = "UPDATE EqManage.log set checkoutDate = '$today' where users_id = '$userID' AND checkoutDate IS NULL AND returnDate IS NULL and equipment_id = '$eqID'";
+        $updateEquipment = "Update EqManage.equipment set popularity = popularity + 1 where id = '$eqID'";
+    } else $query = "UPDATE EqManage.log set checkoutDate = '$today' where checkoutRequests_id = '$requestOption' and equipment_id = '$eqID'";
 
+    $updateEquipment = "Update EqManage.equipment set popularity = popularity + 1 where id = '$eqID'";
 
     if (mysqli_query($db, $query)) {
+        echo "Successfully updated table";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($db);
+    }
+
+    if (mysqli_query($db, $updateEquipment)) {
         echo "Successfully updated table";
     } else {
         echo "Error: " . $query . "<br>" . mysqli_error($db);
--- adminReturn.php
+++ adminReturn.php
@@ -11,15 +11,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo $eqID;
     echo $requestOption;
 
+    $QtyArray = array();
+    $totalReturnQty = 0;
+    $query = "";
+
+
+
+
     if ($requestOption == 0) {
-        $query = "UPDATE EqManage.log set returnDate = '$today' where users_id = '$userID' AND checkoutDate IS NOT NULL AND returnDate IS NULL and equipment_id = '$eqID'";
-    } else $query = "UPDATE EqManage.log set returnDate = '$today' where checkoutRequests_id = '$requestOption'";
+        $query = "UPDATE EqManage.log set returnDate = '$today' where users_id = '$userID' AND checkoutDate IS NOT NULL AND returnDate IS NULL AND equipment_id = '$eqID'";
 
+        $getCheckoutQty = mysqli_query($db,"select * from EqManage.log l left join requests r on l.checkoutRequests_id = r.id where l.users_id = '$userID' AND l.checkoutDate IS NOT NULL AND l.returnDate IS NULL AND l.equipment_id = '$eqID'");
 
+        while ($row = mysqli_fetch_array($getCheckoutQty)){
+            array_push($QtyArray,$row['checkoutQty']);
+        };
+
+        foreach ($QtyArray as $Qty){
+            $totalReturnQty += $Qty;
+        }
+        $equipmentUpdateQuery = "UPDATE EqManage.equipment set leftQuantity = leftQuantity + '$totalReturnQty' where id = '$eqID'";
+
+    } else {
+        $query = "UPDATE EqManage.log set returnDate = '$today' where checkoutRequests_id = '$requestOption'";
+        $getCheckoutQty = mysqli_query($db,"SELECT * FROM EqManage.log l left join requests r on l.checkoutRequests_id = r.id where checkoutRequests_id = '$requestOption'");
+        while ($row = mysqli_fetch_array($getCheckoutQty)){
+            $totalReturnQty = $row['checkoutQty'];
+        }
+        $equipmentUpdateQuery = "UPDATE EqManage.equipment set leftQuantity = leftQuantity + '$totalReturnQty' where id = '$eqID'";
+    }
+
+    $updateAvailability = "Update EqManage.equipment set availability = 1 where id = '$eqID'"; //It will always be available after return
+
+    //Update Log
     if (mysqli_query($db, $query)) {
         echo "Successfully updated table";
     } else {
-        echo "Error: " . $query . "<br>" . mysqli_error($db);
+        echo mysqli_error($db);
     }
+
+    //Update Equipment
+    if (mysqli_query($db, $equipmentUpdateQuery)) {
+        echo "Successfully updated table";
+    } else {
+        echo mysqli_error($db);
+    }
+
+    if (mysqli_query($db, $updateAvailability)) {
+        echo "Successfully updated table";
+    } else {
+        echo mysqli_error($db);
+    }
+
 
 }
