89c32812085e8a1a1b3fbe8d909f73a8e191629b
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-02 00:24:52 +0800

    added dynamic navbar + various visual changes + added checkout/return modal access

--- requests.php
+++ requests.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
-if(!isset($_SESSION['loggedin'])) {
+if(!isset($_SESSION['loggedin'])){
     header('Location: login.php');
     exit();
 }
+if ($_SESSION['username'] != 'administrator'){
+    header('Location: new_index.php?adminonly=1');
+}
+
 include ('serverconnect.php');
 ?>
 <!DOCTYPE html>
@@ -17,16 +21,21 @@ include('header.php')
     <div class="loader"><div></div><div></div><div></div><div></div></div>
 </div>
 
-<?php include('navbar.php'); ?>
+<?php if ($_SESSION['username'] == 'administrator'){
+    include ('adminNavbar.php');
+} else{
+    include ('navbar.php');
+}
+?>
 
 <div class="content">
-
+    <div style="height: 63px; opacity: 0; padding: 0; margin: 0" ></div>
     <?php if (isset($_GET['verify']) && $_GET['verify'] == 1){
-        echo '<p style="color: red" >Successfully Verified</p>';
+        echo '<p style="color: green" >Successfully Verified</p>';
     } ?>
 
 
-    <div class="limiter" style="padding-top: 0; margin-top: 7%">
+    <div class="limiter" style="padding-top: 0;">
         <h2 style="padding-bottom: 10px; margin-bottom: 20px">All Requests</h2>
         <label for="select-box1" class="label select-box1"><span class="label-desc">Show: </span> </label>
         <input type="checkbox" id="approved" name="approved" value="approved" style="margin-left: 5px" onchange="changeCheckbox();" checked>
@@ -63,9 +72,16 @@ include('header.php')
             </div>
         </div>
     </div>
-
-
 </div>
+
+<?php
+
+if ($_SESSION['username'] == 'administrator'){
+    include ('adminModal.php');
+}
+
+?>
+
 
 <script>
 function changeCheckbox() {
@@ -98,8 +114,9 @@ window.onload = function () {
     changeCheckbox();
 }
 
-</script>
 
+
+</script>
 
 </body>
 
