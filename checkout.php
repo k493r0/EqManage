1fb4ad63f121882f4533e502cc14d6d55bd045ed
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-02 00:37:03 +0800

    reverted access privilege

--- checkout.php
+++ checkout.php
@@ -4,9 +4,6 @@ if(!isset($_SESSION['loggedin'])){
     header('Location: login.php');
     exit();
 }
-if ($_SESSION['username'] != 'administrator'){
-    header('Location: new_index.php?adminonly=1');
-}
 include ('serverconnect.php');
 ?>
 <!DOCTYPE html>
