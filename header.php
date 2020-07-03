a022c0859e9137a7e645aee613382b5a207a6d20
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-03 15:45:12 +0800

    working notification system

--- header.php
+++ header.php
@@ -8,21 +8,23 @@
     <!-- Main Style Css -->
     <link rel="stylesheet" href="assets/css/loaderstyle.css"/>
     <link rel="stylesheet" href="assets/css/corestyle.css"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+<!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">-->
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/tablestyle.css">
     <link rel="stylesheet" href="assets/css/modalstyle.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
 
-    <script src="assets/js/scripts.js" type="text/javascript"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
     <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+
 <!--    <script src="assets/js/jquery.min.js"></script>-->
     <script src="assets/js/select2.min.js"></script>
     <link rel="stylesheet" href="assets/css/select2.min.css"/>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/canvasjs.min.js"></script>
-
+    <script src="assets/js/scripts.js" type="text/javascript"></script>
     <script>
         $(function () {
             setTimeout(function () {
