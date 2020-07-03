ab9e11796a225ffe086ae2748722f06ce4c6df59
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-03 22:55:10 +0800

    working notify system from overdue

---
+++ notifyOverdue.php
@@ -0,0 +1,91 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'assets/src/Exception.php';
+require 'assets/src/PHPMailer.php';
+require 'assets/src/SMTP.php';
+
+include('serverconnect.php');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['notify']) && $_POST['notify'] == 1){
+        $id = $_POST['id'];
+        $today = date("Y-m-d H:i:s");
+        echo $id;
+
+        $query = mysqli_query($db, "select requests.users_id, equipment.equipment from EqManage.requests left join EqManage.equipment on requests.equipment_id = equipment.id where requests.id = '$id'");
+        while ($row = mysqli_fetch_array($query)) {
+            $eqname = $row['equipment'];
+            echo $eqname;
+            $userID = $row['users_id'];
+        }
+        echo $userID;
+
+        $notif_query = "INSERT into EqManage.notification (message,target,status,datetime) values ( '$eqname is overdue. Please return it as soon as possible','$userID',0, '$today')";
+        if (mysqli_query($db, $notif_query)) {
+            $last_id = mysqli_insert_id($db);
+            echo "Notification updated. Last inserted ID is: " . $last_id;
+        } else {
+            echo "Error: " . $notif_query . "<br>" . mysqli_error($db);
+        }
+
+
+
+
+        $sql = "SELECT email FROM users WHERE id = ?";
+        if ($stmt = mysqli_prepare($db, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "i", $param_id);
+
+            // Set parameters
+            $param_id = $userID;
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                $result = mysqli_stmt_get_result($stmt);
+                while ($row = mysqli_fetch_assoc($result)){
+                    $email = $row['email'];
+                    echo "helo";
+                }
+
+            } else {
+                echo "Oops! Something went wrong. Please try again later";
+            }
+            mysqli_stmt_close($stmt);
+        }
+
+        echo "e".$email;
+
+
+        $mail = new PHPMailer;
+
+        $mail->isSMTP();                            // Set mailer to use SMTP
+        $mail->Host = '***REMOVED***';             // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                     // Enable SMTP authentication
+        $mail->Username = '***REMOVED***';          // SMTP username
+        $mail->Password = '***REMOVED***'; // SMTP password
+        $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = ***REMOVED***;                          // TCP port to connect to
+        // TCP port to connect to
+
+        $mail->setFrom('***REMOVED***', 'Notification System');
+        $mail->addAddress("$email");   // Add a recipient
+
+        $mail->isHTML(true);  // Set email format to HTML
+
+        $bodyContent = '<p>You have an equipment overdue<br>Please return '.$eqname.' as soon as possible</p>';
+
+        $mail->Subject = 'Equipment Approval';
+        $mail->Body = $bodyContent;
+
+        if (!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message has been sent';
+        }
+
+
+    }
+}
