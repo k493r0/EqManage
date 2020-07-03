c0c79c7fb157c4c8337694dc0bbd92b99fc50ff2
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-03 18:23:49 +0800

    changed pdf filename

--- barcodePDF.php
+++ barcodePDF.php
@@ -112,7 +112,7 @@ $pdf->lastPage();
 // ---------------------------------------------------------
 
 //Close and output PDF document
-$pdf->Output('example_006.pdf', 'I');
+$pdf->Output('barcodee.pdf', 'I');
 
 //============================================================+
 // END OF FILE
