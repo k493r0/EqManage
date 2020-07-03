d84c2eb48b0fc155c96b3d7a78aedf02d9de647c
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-03 21:17:46 +0800

    added ability to delete equipment + enabled select refresh when adding equipment + added ability to display and delete category

--- addEq.php
+++ addEq.php
@@ -1,99 +1,112 @@
 <?php
 include('serverconnect.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['add']) && $_POST['add'] == 1) {
+
+        //    $user = $_SESSION['name'];
+        //    $equipment = $_POST['equipment'];
+        //    $notes = $_POST['purpose'];
+        //    $hash = md5(rand(0, 1000));
+        //    $requestdate = date('Y-m-d H:i:s');
+
+        $name = $_POST['name'];
+        $quantity = $_POST['quantity'];
+        $category = $_POST['category_id'];
+        $new_category = $_POST['other'];
 
 
-//    $user = $_SESSION['name'];
-//    $equipment = $_POST['equipment'];
-//    $notes = $_POST['purpose'];
-//    $hash = md5(rand(0, 1000));
-//    $requestdate = date('Y-m-d H:i:s');
+        $randomNumber = mt_rand(10000000, 99999999);
 
-    $name = $_POST['name'];
-    $quantity = $_POST['quantity'];
-    $category = $_POST['category_id'];
-    $new_category = $_POST['other'];
+        //    echo $name;
+        //    echo $quantity;
+        //    echo $new_category;
+
+        if ($new_category == NULL && $category != NULL) { //If cateogory is selected and no new category is made
+            $query1 = "insert into EqManage.equipment (equipment,category,totalQuantity,leftQuantity,barcodeID) values ('$name','$category','$quantity','$quantity','$randomNumber')";
+            if (mysqli_query($db, $query1)) {
+                echo "Successfully added equipment";
+            } else {
+                echo "Error: " . $query1 . "<br>" . mysqli_error($db);
+            }
+        } elseif ($new_category != NULL) { //If new category is made
+            $cat_query = "Insert into EqManage.categories (categoryName) values ('$new_category')";
+            if (mysqli_query($db, $cat_query)) {
+                $last_id = mysqli_insert_id($db); //The inserted item's ID
+                echo "New record created successfully. Last inserted ID is: " . $last_id;
+            } else {
+                echo "Error: " . $cat_query . "<br>" . mysqli_error($db);
+            }
 
 
-    $randomNumber = mt_rand(10000000,99999999);
-
-//    echo $name;
-//    echo $quantity;
-//    echo $new_category;
-
-    if ($new_category == NULL && $category != NULL){ //If cateogory is selected and no new category is made
-        $query1 = "insert into EqManage.equipment (equipment,category,totalQuantity,leftQuantity,barcodeID) values ('$name','$category','$quantity','$quantity','$randomNumber')";
-        if (mysqli_query($db, $query1)) {
-            echo "Successfully added equipment";
-        } else {
-            echo "Error: " . $query1 . "<br>" . mysqli_error($db);
+            $query2 = "insert into EqManage.equipment (equipment,category,totalQuantity,leftQuantity,barcodeID) values ('$name','$last_id','$quantity','$quantity','$randomNumber')";
+            if (mysqli_query($db, $query2)) {
+                echo "Successfully added equipment into database";
+            } else {
+                echo "Error: " . $query2 . "<br>" . mysqli_error($db);
+            }
+            exit;
         }
-    } elseif ($new_category != NULL){ //If new category is made
-        $cat_query = "Insert into EqManage.categories (categoryName) values ('$new_category')";
-        if (mysqli_query($db, $cat_query)) {
-            $last_id = mysqli_insert_id($db); //The inserted item's ID
-            echo "New record created successfully. Last inserted ID is: " . $last_id;
-        } else {
-            echo "Error: " . $cat_query . "<br>" . mysqli_error($db);
-        }
 
 
-
-        $query2 = "insert into EqManage.equipment (equipment,category,totalQuantity,leftQuantity,barcodeID) values ('$name','$last_id','$quantity','$quantity','$randomNumber')";
+    }
+    if (isset($_POST['remove']) && $_POST['remove'] == 1 && $_POST['type'] == 1){
+        $eqID = $_POST['id'];
+        $query2 = "Delete from EqManage.equipment where id="."$eqID";
+        echo $query2;
         if (mysqli_query($db, $query2)) {
-            echo "Successfully added equipment into database";
+            echo "Successfully removed equipment from the database";
         } else {
             echo "Error: " . $query2 . "<br>" . mysqli_error($db);
         }
         exit;
     }
 
+    if (isset($_POST['remove']) && $_POST['remove'] == 1 && $_POST['type'] == 2){
+        $catID = $_POST['id'];
+        $query2 = "Delete from EqManage.categories where id="."$catID";
+        echo $query2;
+        if (mysqli_query($db, $query2)) {
+            echo "Successfully removed category from the database";
+        } else {
+            echo "Error: " . $query2 . "<br>" . mysqli_error($db);
+        }
+        exit;
+    }
+
+    if (isset($_POST['display']) && $_POST['display'] == 1) {
 
 
+        $results = mysqli_query($db, "SELECT equipment.id, equipment, totalQuantity, leftQuantity, users_id, lastLog_id, categoryName FROM EqManage.equipment inner join EqManage.categories on equipment.category = categories.id");
+        while ($row = mysqli_fetch_array($results)) {
 
-
-//
-//
-//
-//
-//
-//    echo $equipment;
-//    $query = "INSERT INTO allrequests (User,Equipment,Notes,Hash,Action) VALUES ('$user','$equipment','$notes','$hash', 'Check-Out')";
-//
-//    mysqli_query($db, $query);
-}
-
-if ($_POST['display'] && $_POST['display'] == 1){
-
-
-$results = mysqli_query($db, "SELECT * FROM EqManage.equipment inner join EqManage.categories on equipment.category = categories.id");
-while ($row = mysqli_fetch_array($results)) {
-
-    $catName = $row['categoryName'];
-    $tqty = $row['totalQuantity'];
-
-    ?>
-    <tr>
-        <td><?php echo $row['equipment']; ?></td>
-        <td><?php echo "<a href='#'>$catName</a>";?></td>
-        <td><?php echo $row['totalQuantity']; ?></td>
-        <td><?php echo $row['leftQuantity']; ?></td>
-        <td>
-            <?php
-
-            if ($row['leftQuantity'] >= 1) {
-                echo "Available";
-            } elseif ($row['leftQuantity'] <= 0){
-                echo "Not Available";
-            } else echo "Error";
+            $catName = $row['categoryName'];
+            $tqty = $row['totalQuantity'];
 
             ?>
-        </td>
-        <td><?php echo $row['users_id']; ?></td>
-        <td><?php echo $row['lastLog_id']; ?></td>
+            <tr>
+                <td><?php echo $row['equipment']; ?></td>
+                <td><?php echo "<a href='#'>$catName</a>";?></td>
+                <td><?php echo $row['totalQuantity']; ?></td>
+                <td><?php echo $row['leftQuantity']; ?></td>
+                <td>
+                    <?php
+
+                    if ($row['leftQuantity'] >= 1) {
+                        echo "Available";
+                    } elseif ($row['leftQuantity'] <= 0){
+                        echo "Not Available";
+                    } else echo "Error";
+
+                    ?>
+                </td>
+                <td><?php echo $row['users_id']; ?></td>
+                <td><?php echo $row['lastLog_id']; ?></td>
+                <td><button type='button' class='btn btn-link' id="removeEq" style="padding: 0; margin: 0" onclick="removeEq(this.value)" value="<?php echo $row['id']; ?>">Remove</button></td>
+
+            </tr>
+        <?php }
+    }
 
 
-    </tr>
-<?php } }?>
 }
-
+    ?>
