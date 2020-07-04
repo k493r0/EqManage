9b5e3a17d11fc3cddf94f47736e0fe64a3752036
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-05 00:21:25 +0800

    added account verification system + reset password feature

---
+++ resetPassword.php
@@ -0,0 +1,121 @@
+<?php
+// Initialize the session
+session_start();
+include('serverconnect.php');
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Include config file
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Please enter the new password.";
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Password must have atleast 6 characters.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm the password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET password = ? WHERE id = ?";
+
+        if($stmt = mysqli_prepare($db, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                session_start();
+                $_SESSION['msg'] = "Password successfully reset, please login again";
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($db);
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<?php
+include('header.php')
+?>
+<body class="form-v8 loggedin" id="fade">
+
+<div id="loader">
+    <div class="loader"><div></div><div></div><div></div><div></div></div>
+</div>
+
+<?php if ($_SESSION['username'] == 'administrator'){
+    include ('adminNavbar.php');
+} else{
+    include ('navbar.php');
+}
+?>
+
+<div class="content">
+    <div style="height: 63px; opacity: 0; padding: 0; margin: 0" ></div>
+    <?php if (isset($_GET['verify']) && $_GET['verify'] == 1){
+        echo '<p style="color: green" >Successfully Verified</p>';
+    } ?>
+
+
+    <div class="limiter" style="padding-top: 0;">
+        <div class="wrapper">
+            <h2>Reset Password</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                    <label>New Password</label>
+                    <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
+                    <span class="help-block"><?php echo $new_password_err; ?></span>
+                </div>
+                <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirm_password" class="form-control">
+                    <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Reset Password">
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
