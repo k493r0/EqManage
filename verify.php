c434684ae0cecc024c1241cde59e30f4bc6a1804
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-05 21:50:35 +0800

    added back redirection in verify.php

--- verify.php
+++ verify.php
@@ -229,14 +229,14 @@ if ($mode == '1') {
 
 
 
-//$headerRefer = substr($referer,strrpos($referer,'/') + 1);
-//echo $headerRefer;
-//
-//if ($referer != null){
-//    header("Location: $headerRefer?verify=1" );
-//} elseif ($referer == null){
-//    header('Location: new_index.php?verify=1');
-//} else header('Location: new_index.php');
+$headerRefer = substr($referer,strrpos($referer,'/') + 1);
+echo $headerRefer;
+
+if ($referer != null){
+    header("Location: $headerRefer?verify=1" );
+} elseif ($referer == null){
+    header('Location: new_index.php?verify=1');
+} else header('Location: new_index.php');
 
 
 
