fbff8e1f36072fdefed998fe23db5d5d33fa12d0
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-05 13:16:41 +0800

    added notification for admins

--- adminNavbar.php
+++ adminNavbar.php
@@ -9,7 +9,7 @@
 
                     <li class="dropdown nav-item">
                         <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Administrative Tools</a>
-                        <div class="dropdown-menu" role="menu">
+                        <div class="dropdown-menu" role="menu" style="border-radius: 8px; margin-top:15px; margin-left: -10px">
                             <a class="dropdown-item" role="presentation" href="dashboard.php">Dashboard</a>
                             <a class="dropdown-item" role="presentation" href="requests.php">Requests</a>
                             <a class="dropdown-item" role="presentation" href="log.php">Log</a>
@@ -19,7 +19,21 @@
                             <a class="dropdown-item" role="presentation" href="search.php">Search</a>
                         </div>
                     </li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="logout.php" style="color: salmon;">Logout</a></li>
+
+
+                    <li class="dropdown nav-item">
+                        <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Account</a>
+                        <div class="dropdown-menu" role="menu" style="border-radius: 8px; margin-top:15px; margin-left: -80px">
+                            <a class="dropdown-item" role="presentation" href="resetPassword.php">Reset Password</a>
+                            <a class="dropdown-item" href="logout.php" style="color: salmon;">Logout</a>
+                        </div>
+                    </li>
+
+                    <li class="dropdown" style="margin-left:10px" id="notif-dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" onclick="load_unseen_notification('read')"><span class="badge" style="position: absolute;left: 13px;top: 2px;color: white;background: red;" id="countBadge">0</span>
+                            <span class="material-icons" style="padding-top:20%">notifications</span></a>
+                        <ul class="dropdown-menu-notif dropdown-menu" style="padding: 10px; min-width:300px;max-height: 50vh; height: auto;overflow-y: auto; border-radius: 8px; margin-top:15px" id="notif-drop"></ul>
+                    </li>
                 </ul><span class="navbar-text actions"> <a href="#returnModal" data-toggle="modal" class="login" data-backdrop="false" id="returnModalBtn">Return</a><a class="btn btn-light action-button" data-toggle="modal" role="button" href="#checkoutModal" data-backdrop="false" id="checkoutModalBtn">Scan & Checkout</a></span></div>
         </div>
     </nav>
