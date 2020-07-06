76fd6e9882a40cd390d77acadcf07856f1ced199
Wren Hainsworth <hainsworthwren@gmail.com>
2020-07-06 14:25:37 +0800

    transferred new_index to index + spelling fix + added fullname in session on login

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: new_index.php");
+    header("location: index.php");
     exit;
 }
 include ('serverconnect.php');
@@ -101,7 +101,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 $password_err = "Please enter a password";
                 $_SESSION['error'] .= $password_err." | ";
             } elseif (strlen(trim(mysqli_real_escape_string($db, $_POST["regPassword_1"]))) < 6) {
-                $password_err = "Password must have atleast 6 characters";
+                $password_err = "Password must have at least 6 characters";
                 $_SESSION['error'] .= $password_err." | ";
             } else {
                 $password = trim(mysqli_real_escape_string($db, $_POST["regPassword_1"]));
@@ -207,7 +207,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             // Validate credentials
             if (empty($username_err) && empty($password_err)) {
                 // Prepare a select statement
-                $sql = "SELECT id, username, password FROM users WHERE username = ?";
+                $sql = "SELECT id, username, password, fullname FROM users WHERE username = ?";
 
                 if ($stmt = mysqli_prepare($db, $sql)) {
                     // Bind variables to the prepared statement as parameters
@@ -220,11 +220,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                     if (mysqli_stmt_execute($stmt)) {
                         // Store result
                         mysqli_stmt_store_result($stmt);
-
                         // Check if username exists, if yes then verify password
                         if (mysqli_stmt_num_rows($stmt) == 1) {
                             // Bind result variables
-                            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $fullname);
                             if (mysqli_stmt_fetch($stmt)) {
                                 if (password_verify($password, $hashed_password)) {
                                     // Password is correct, so start a new session
@@ -234,13 +233,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                                     $_SESSION["loggedin"] = true;
                                     $_SESSION["id"] = $id;
                                     $_SESSION["username"] = $username;
+                                    $_SESSION["fullname"] = $fullname;
                                     unset($_SESSION['error']);
                                     // Redirect user to welcome page
 
                                     if ($_SESSION['username'] == 'administrator'){
                                         header("location: dashboard.php");
                                     } else{
-                                        header("location: new_index.php");
+                                        header("location: index.php");
                                     }
                                 } else {
                                     // Display an error message if password is not valid
